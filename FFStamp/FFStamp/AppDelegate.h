59de9ce09430de74c4ff3fd65e216cdb2ea2915a
AlexNiu <allanniu2012@gmail.com>
2016-01-06 15:01:33 +0800

    添加FFStamp工程

---
+++ FFStamp/FFStamp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FFStamp
+//
+//  Created by Yong.J.N on 16/1/6.
+//  Copyright © 2016年 Yong.J.N. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FFStamp/FFStamp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FFStamp
+//
+//  Created by Yong.J.N on 16/1/6.
+//  Copyright © 2016年 Yong.J.N. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
